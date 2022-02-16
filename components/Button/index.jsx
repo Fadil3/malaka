4e778c0ff589280a5cb12a4db03561b49f1b975a
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-02-16 20:16:53 +0700

    create  button component

---
+++ components/Button/index.jsx
@@ -0,0 +1,25 @@
+const Button = ({ className, children, variant, href, submit }) => {
+  const addClasName = className ? `${className} ` : ''
+  const variants = {
+    white: 'bg-white text-black ',
+  }
+  const baseStyling =
+    'transition py-3.5 px-7 rounded-full font-semibold text-lg inline-block '
+  const pickedVariant = variants[variant]
+  const mergedStyle = `${baseStyling} ${pickedVariant} ${addClasName}`
+  return (
+    <>
+      {submit ? (
+        <button className={mergedStyle} type="submit">
+          {children}
+        </button>
+      ) : (
+        <a className={mergedStyle} href={href ? href : '#'}>
+          {children}
+        </a>
+      )}
+    </>
+  )
+}
+
+export default Button
