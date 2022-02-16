32725b041a272c9678ea8bee7e71424d5af8fd76
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-02-16 20:19:51 +0700

    create navbar

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,10 +1,16 @@
+import Button from '../Button'
 import Logo from './Logo'
+import Nav from './Nav'
 
 export default function Navbar() {
   return (
     <>
-      <nav>
+      <nav className="sticky flex justify-between items-center bg-dark p-2">
         <Logo />
+        <Nav />
+        <Button href="#donwload" variant="white">
+          Download
+        </Button>
       </nav>
     </>
   )
