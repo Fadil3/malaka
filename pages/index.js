69d82b6f1e34d6db889c741d733a0e43675ad1a5
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-02-17 08:09:35 +0700

    adjust hero bg

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="bg-dark px-32 border-b-8 h-[750px] rounded-br-[140px] pt-2 relative">
+      <div className="bg-dark px-32 border-b-8 h-[650px] rounded-br-[140px] pt-2 relative">
         <Hero />
       </div>
       <div className="bg-dark -mt-20 -skew-y-6 h-32 rounded-br-[130px]"></div>
