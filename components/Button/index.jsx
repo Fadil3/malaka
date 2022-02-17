cd9039c9db3479329b5107b92c5ecf4c19c4ca2d
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-02-17 07:12:41 +0700

    add button variant

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -2,6 +2,7 @@ const Button = ({ className, children, variant, href, submit }) => {
   const addClasName = className ? `${className} ` : ''
   const variants = {
     white: 'bg-white text-black ',
+    primary: 'bg-primary text-white ',
   }
   const baseStyling =
     'transition py-3.5 px-7 rounded-full font-semibold text-lg inline-block '
