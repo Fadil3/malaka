b2ed84e4226a9ccfd70311dd781f8c6a6a7e7fe6
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-02-22 08:23:52 +0700

    add benefit to home

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,22 @@ import For from '../components/For'
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
 import Introduction from '../components/Introduction'
+import Benefit from '../components/Benefit'
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="bg-dark px-32 border-b-8 h-[650px] rounded-br-[140px] pt-2 relative">
+      <div className="bg-dark px-20 border-b-8 h-[650px] rounded-br-[140px] pt-2 relative">
         <Hero />
       </div>
 
       <div className="bg-dark -mt-20 -skew-y-6 h-32 rounded-br-[130px]" />
-      <div className="mx-32">
+      <div className="mx-20">
         <For />
       </div>
       <Introduction />
+      <Benefit />
     </>
   )
 }
