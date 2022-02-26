e40fb95829b217c7f1c511777ee212261171a009
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-02-26 21:01:38 +0700

    add other benefit text

--- components/Benefit/index.jsx
+++ components/Benefit/index.jsx
@@ -10,7 +10,7 @@ export default function Benefit() {
           <p className=" text-2xl font-medium">
             Manfaat yang kamu dapatkan ketika membeli buku Tailwind CSS ini
           </p>
-          <p className=" text-lg opacity-80">
+          <p className=" text-lg opacity-80 mt-5">
             &quot;Buku itu seperti cermin: kalau yang berkaca padanya adalah
             seorang yang bodoh, engkau tak bisa berharap yang terpantul adalah
             seorang yang jenius.&quot; - J.K Rowling
@@ -24,6 +24,27 @@ export default function Benefit() {
         </div>
         <div className="w-5/12">
           <Image src="/benefit.png" alt="benefit" width={575} height={599} />
+        </div>
+      </div>
+      <div className="flex justify-between mt-24 px-20">
+        <div className="w-5/12 drop-shadow-2xl -rotate-6">
+          <Image
+            src="/other-benefit.png"
+            alt="other-benefit"
+            width={450}
+            height={500}
+          />
+        </div>
+        <div className="w-6/12 ">
+          <p className="text-primary font-bold">SELAIN ITU</p>
+          <p className=" text-2xl font-medium">
+            Buku dengan studi kasus nyata, juga komunitas yang ramah
+          </p>
+          <p className=" text-lg opacity-80 mt-5">
+            Buku ini diakhiri dengan membangun website yang responsive dengan
+            Tailwind. Selain itu kamu juga kamu dapat bergabung ke komunitas
+            untuk bertanya ketika mengalami kesulitan.
+          </p>
         </div>
       </div>
     </>
